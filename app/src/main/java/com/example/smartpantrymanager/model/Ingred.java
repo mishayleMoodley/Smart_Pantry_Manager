ec4510c68def100b1d4d67ee1492aab15965937e
Mishayle Moodley <mishayle06@gmail.com>
2026-09-16 10:03:45 +0200

    added Ingred.java and IngredNormalization.java classes (will normalize data for eg, grams to g and will make names singular), as well as Recipe.java and RecipeIngred.java classes

---
+++ app/src/main/java/com/example/smartpantrymanager/model/Ingred.java
@@ -0,0 +1,4 @@
+package com.example.smartpantrymanager.model;
+
+public class Ingred {
+}
---
+++ app/src/main/java/com/example/smartpantrymanager/util/IngredNormalization.java
@@ -0,0 +1,6 @@
+package com.example.smartpantrymanager.util;
+
+public class IngredNormalization {
+    // used to normalize units (example grams to g)
+    // and changing from plural to singuler (example tomatoes to tomato)
+}
